e9f9bf994ee9351bb1cc4a04a980695fcc505bf6
Karl Nosworthy <karl@Karls-MacBook-Pro.local>
2016-05-18 00:30:21 +0100

    Updated service to use new Retrofit 2 URL scheme and generic based Call returns.

--- src/main/java/com/karlnosworthy/freeagent/FreeAgentService.java
+++ src/main/java/com/karlnosworthy/freeagent/FreeAgentService.java
@@ -1,104 +1,105 @@
 package com.karlnosworthy.freeagent;
 
-import com.karlnosworthy.freeagent.model.FreeAgentCompany;
 import com.karlnosworthy.freeagent.model.FreeAgentUser;
 import com.karlnosworthy.freeagent.model.wrapper.*;
-import retrofit.client.Response;
-import retrofit.http.*;
+import retrofit2.Call;
+
+import retrofit2.Response;
+import retrofit2.http.*;
 
 public interface FreeAgentService {
 
     //
     // ==== Company ====
     //
-    @GET("/company")
-    FreeAgentCompanyWrapper getCompany();
+    @GET("company")
+    Call<FreeAgentCompanyWrapper> getCompany();
 
     //
     // ==== Contacts ====
     //
-    @GET("/contacts/")
-    FreeAgentContactWrapper getContacts();
+    @GET("contacts/")
+    Call<FreeAgentContactWrapper> getContacts();
 
-    @GET("/contacts/")
-    FreeAgentContactWrapper getContacts(@Query("view") String viewFilter);
+    @GET("contacts/")
+    Call<FreeAgentContactWrapper> getContacts(@Query("view") String viewFilter);
 
-    @GET("/contacts/")
-    FreeAgentContactWrapper getContacts(@Query("view") String viewFilter, @Query("sort") String sortOrder);
+    @GET("contacts/")
+    Call<FreeAgentContactWrapper> getContacts(@Query("view") String viewFilter, @Query("sort") String sortOrder);
 
-    @GET("/contacts/{id}")
-    FreeAgentContactWrapper getContact(@Path("id") String contactId);
+    @GET("contacts/{id}")
+    Call<FreeAgentContactWrapper> getContact(@Path("id") String contactId);
 
-    @POST("/contacts")
-    FreeAgentContactWrapper createContact(@Body FreeAgentContactWrapper contact);
+    @POST("contacts")
+    Call<FreeAgentContactWrapper> createContact(@Body FreeAgentContactWrapper contact);
 
-    @PUT("/contacts/{id}")
+    @PUT("contacts/{id}")
     Response updateContact(@Body FreeAgentContactWrapper contact, @Path("id") String contactId);
 
-    @DELETE("/contacts/{id}")
+    @DELETE("contacts/{id}")
     Response deleteContact(@Path("id") String contactId);
 
     //
     // ==== Invoices ====
     //
-    @GET("/invoices")
-    FreeAgentInvoiceWrapper getInvoices(@Query("nested_invoice_items") boolean nestInvoiceItems);
+    @GET("invoices")
+    Call<FreeAgentInvoiceWrapper> getInvoices(@Query("nested_invoice_items") boolean nestInvoiceItems);
 
-    @GET("/invoices/{id}")
-    FreeAgentInvoiceWrapper getInvoice(@Path("id") String invoiceId);
+    @GET("invoices/{id}")
+    Call<FreeAgentInvoiceWrapper> getInvoice(@Path("id") String invoiceId);
 
-    @POST("/invoices")
-    FreeAgentInvoiceWrapper createInvoice(@Body FreeAgentInvoiceWrapper invoice);
+    @POST("invoices")
+    Call<FreeAgentInvoiceWrapper> createInvoice(@Body FreeAgentInvoiceWrapper invoice);
 
-    @PUT("/invoices/{id}")
+    @PUT("invoices/{id}")
     Response updateInvoice(@Body FreeAgentInvoiceWrapper invoice, @Path("id") String invoiceId);
 
-    @DELETE("/invoices/{id}")
+    @DELETE("invoices/{id}")
     Response deleteInvoice(@Path("id") String invoiceId);
 
     //
     // ==== Projects ====
     //
-    @GET("/projects/")
-    FreeAgentProjectWrapper getProjects();
+    @GET("projects/")
+    Call<FreeAgentProjectWrapper> getProjects();
 
-    @GET("/projects/")
-    FreeAgentProjectWrapper getProjects(@Query("view") String viewFilter);
+    @GET("projects/")
+    Call<FreeAgentProjectWrapper> getProjects(@Query("view") String viewFilter);
 
-    @GET("/projects")
-    FreeAgentProjectWrapper getProjectsForContact(@Query("contact") String contactURL);
+    @GET("projects")
+    Call<FreeAgentProjectWrapper> getProjectsForContact(@Query("contact") String contactURL);
 
-    @GET("/projects/{id}")
-    FreeAgentProjectWrapper getProject(@Path("id") String projectId);
+    @GET("projects/{id}")
+    Call<FreeAgentProjectWrapper> getProject(@Path("id") String projectId);
 
-    @POST("/projects")
-    FreeAgentProjectWrapper createProject(@Body FreeAgentProjectWrapper project);
+    @POST("projects")
+    Call<FreeAgentProjectWrapper> createProject(@Body FreeAgentProjectWrapper project);
 
-    @PUT("/projects/{id}")
+    @PUT("projects/{id}")
     Response updateProject(@Body FreeAgentProjectWrapper project, @Path("id") String projectId);
 
-    @DELETE("/projects/{id}")
+    @DELETE("projects/{id}")
     Response deleteProject(@Path("id") String projectId);
 
     //
     // ==== Users ====
     //
-    @GET("/users/")
-    FreeAgentUserWrapper getUsers();
+    @GET("users/")
+    Call<FreeAgentUserWrapper> getUsers();
 
-    @GET("/users/{id}")
-    FreeAgentUserWrapper getUser(@Path("id") String userId);
+    @GET("users/{id}")
+    Call<FreeAgentUserWrapper> getUser(@Path("id") String userId);
 
-    @GET("/users/me")
-    FreeAgentUserWrapper getCurrentUser();
+    @GET("users/me")
+    Call<FreeAgentUserWrapper> getCurrentUser();
 
-    @POST("/users")
-    FreeAgentUserWrapper createUser(@Body FreeAgentUserWrapper user);
+    @POST("users")
+    Call<FreeAgentUserWrapper> createUser(@Body FreeAgentUserWrapper user);
 
-    @PUT("/users/{id}")
-    Response updateUser(@Body FreeAgentUserWrapper user, @Path("id") String userId);
+    @PUT("users/{id}")
+    Response<FreeAgentUser> updateUser(@Body FreeAgentUserWrapper user, @Path("id") String userId);
 
-    @DELETE("/users/{id}")
+    @DELETE("users/{id}")
     Response deleteUser(@Path("id") String userId);
 }
 
